355ded4423348bf13ad0effa225154099a495017
Pierre Guillen <pierrealexis61196@gmail.com>
2019-04-15 20:51:42 -0500

    act

--- lib/main.dart
+++ lib/main.dart
@@ -9,24 +9,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Resaurant Mobile App',
+      title: 'FLUREST',
       home: MyHomePage(),
     );
   }
 }
-
-// IMAGES
-
-var meatImage =
-    'https://images.unsplash.com/photo-1532597311687-5c2dc87fff52?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80';
-var foodImage =
-    'https://images.unsplash.com/photo-1520218508822-998633d997e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80';
-
-var burgerImage =
-    'https://images.unsplash.com/photo-1534790566855-4cb788d389ec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80';
-var chickenImage =
-    'https://images.unsplash.com/photo-1532550907401-a500c9a57435?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80';
-
 // COLORS
 var textYellow = Color(0xFFf6c24d);
 var iconYellow = Color(0xFFf4bf47);
